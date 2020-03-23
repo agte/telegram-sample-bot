270d901382854b265596b550d28c1a516c874181
Anna Agte <matiasrustagte@gmail.com>
2020-03-23 21:43:55 +0500

    feat: send messages from the bot to a user

--- src/Application.js
+++ src/Application.js
@@ -1,6 +1,9 @@
 import Koa from 'koa';
 import KoaRouter from 'koa-router';
 import parseBody from 'koa-body';
+import fetch from 'node-fetch';
+
+const API_URL = 'https://api.telegram.org/bot1107998527:AAHhVImW2B4K8RWFcZVQ8bJqlNB3nhJ8TgQ';
 
 export default class Application {
   #koaServer = new Koa();
@@ -23,12 +26,49 @@ export default class Application {
       ctx.body = 'Hello! This is @AnnaAgteBot - a sample telegram bot. It only can get webhooks from Telefram API.';
     });
 
-    router.post('/api/telegram/webhook/1107998527_AAHhVImW2B4K8RWFcZVQ8bJqlNB3nhJ8TgQ', parseBody(), (ctx) => {
+    /*
+    {
+      "update_id": 15,
+      "message":
+      {
+        "message_id": 4,
+        "from": {
+          "id": 536884665,
+          "is_bot": false,
+          "first_name": "Anna",
+          "last_name": "Agte",
+          "username": "annaagte",
+          "language_code": "ru"
+        },
+        "chat": {
+          "id": 536884665,
+          "first_name": "Anna",
+          "last_name": "Agte",
+          "username": "annaagte",
+          "type": "private"
+        },
+        "date": 1584973755,
+        "text": "/start"
+      }
+    }
+    */
+    router.post('/api/telegram/webhook/1107998527_AAHhVImW2B4K8RWFcZVQ8bJqlNB3nhJ8TgQ', parseBody(), async (ctx) => {
       const { update_id: updateId, message } = ctx.request.body;
-      console.log(message);
+      console.log(JSON.stringify(message, null, 2));
       switch (message.text) {
         case '/start':
           console.log('Subscribe to newsletter');
+          const response = await fetch(`${API_URL}/sendMessage`, {
+            method: 'POST',
+            body: new URLSearchParams({
+              chat_id: message.chat.id,
+              text: `Новостей нет. <b>Жирный текст</b> <i>Курсив</i> <a href='https://shikari.do'>Ссылка на сайт</a>`,
+              parse_mode: 'HTML',
+              disable_web_page_preview: true,
+            }),
+          })
+          const jsonResponse = await response.json();
+          console.log(jsonResponse);
           break;
         case '/stop':
           console.log('Unsubscribe');
