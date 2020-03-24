0d320335b1d9817a011ffb563ddadd7702ab2fb7
Anna Agte <matiasrustagte@gmail.com>
2020-03-24 21:22:23 +0500

    feat: add Telegram login widget

--- src/Application.js
+++ src/Application.js
@@ -23,7 +23,20 @@ export default class Application {
     const router = new KoaRouter();
 
     router.get('/', (ctx) => {
-      ctx.body = 'Hello! This is @AnnaAgteBot - a sample telegram bot. It only can get webhooks from Telefram API.';
+      // ctx.body = 'Hello! This is @AnnaAgteBot - a sample telegram bot. It only can get webhooks from Telefram API.';
+      ctx.body = `
+<!doctype html>
+<html>
+<body>
+<script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-login="AnnaAgteBot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+<script type="text/javascript">
+  function onTelegramAuth(user) {
+    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+  }
+</script>
+</body>
+</html>
+      `;
     });
 
     /*
